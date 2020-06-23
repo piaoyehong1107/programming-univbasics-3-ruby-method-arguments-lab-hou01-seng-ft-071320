0c274fa55d3db74354271dc8d2414ddb5cfc76ef
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-23 04:45:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,12 @@ def introduction(name)
 end
 
 def introduction_with_language(name, laguage)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, laguage="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
